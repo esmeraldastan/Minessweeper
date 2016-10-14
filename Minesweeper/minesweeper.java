43f391137b25425c1c9595eff5d1461d8e1ee3db
Chase <esmeraldarekz99@gmail.com>
2016-10-14 12:57:36 -0700

    start

---
+++ Minesweeper/minesweeper.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class minesweeper here.
+ * 
+ * @author Esmeralda Lopez 
+ * @version 10.14.16
+ */
+//constructor
+//print array
+//generate bombs
+//add numbers (top, bottom, left, right, )
+//add coner numbers 
+public class minesweeper{
+    public Array(int board){
+        
+    }
+}
+    
